5caad40305ea753e0cd90117295dfe73d9119124
a914-gowtham <a914gowtham@gmail.com>
2021-12-19 17:03:40 +0530

    update: compose version to 1.0.5

--- app/src/main/java/com/gowtham/compose_ratingbar/MApplication.kt
+++ app/src/main/java/com/gowtham/compose_ratingbar/MApplication.kt
@@ -1,11 +1,6 @@
 package com.gowtham.compose_ratingbar
 
 import android.app.Application
-import com.instacart.library.truetime.TrueTime
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.util.logging.Handler
 
 class MApplication : Application() {
 
--- app/src/main/java/com/gowtham/compose_ratingbar/MainActivity.kt
+++ app/src/main/java/com/gowtham/compose_ratingbar/MainActivity.kt
@@ -7,26 +7,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.LayoutDirection
-import androidx.compose.ui.unit.dp
-import com.gowtham.compose_ratingbar.MainActivity.Companion.initialRating
 import com.gowtham.compose_ratingbar.ui.theme.JetpackComposeTheme
 import com.gowtham.ratingbar.RatingBar
 import com.gowtham.ratingbar.RatingBarStyle
-import com.gowtham.ratingbar.StepSize
-import com.instacart.library.truetime.TrueTime
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.util.*
 
 class MainActivity : ComponentActivity() {
 
