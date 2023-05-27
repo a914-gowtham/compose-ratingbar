e03991cee2786bde18ea9cf0c8c598461f0c685f
gowtham.balamurugan <gowtham.balamurugan@zohocorp.com>
2023-05-27 18:57:08 +0530

    update: half star demo

--- app/src/main/java/com/gowtham/compose_ratingbar/MainActivity.kt
+++ app/src/main/java/com/gowtham/compose_ratingbar/MainActivity.kt
@@ -17,11 +17,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gowtham.compose_ratingbar.MainActivity.Companion.initialRating
 import com.gowtham.compose_ratingbar.ui.theme.JetpackComposeTheme
 import com.gowtham.ratingbar.RatingBar
 import com.gowtham.ratingbar.RatingBarConfig
 import com.gowtham.ratingbar.RatingBarStyle
+import com.gowtham.ratingbar.StepSize
 
 class MainActivity : ComponentActivity() {
 
@@ -59,6 +61,9 @@ fun MainScreen() {
         RatingBar(
             value = rating,
             config = RatingBarConfig()
+                .padding(8.dp)
+                .size(28.dp)
+                .stepSize(StepSize.HALF)
                 .style(RatingBarStyle.HighLighted),
             onValueChange = {
                 rating = it
